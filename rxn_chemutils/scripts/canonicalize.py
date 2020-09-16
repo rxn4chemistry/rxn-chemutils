0f3bf418479e1dad562f57b06def656a8cb0eaad
Alain Vaucher <ava@zurich.ibm.com>
2020-09-16 11:14:41 +0200

    improve canoncalize script - now also works with stdin

--- rxn_chemutils/scripts/canonicalize.py
+++ rxn_chemutils/scripts/canonicalize.py
@@ -1,31 +1,24 @@
-import click
+import fileinput
 
 from rxn_chemutils.conversion import canonicalize_smiles, canonicalize_reaction_smiles
 
 
-@click.command()
-@click.option(
-    '--input_file',
-    '-i',
-    required=True,
-    help='File containing the SMILES strings to canonicalize (one per line)'
-)
-@click.option(
-    '--output_file', '-o', required=True, help='File where to save the canonical SMILES strings'
-)
-def canonicalize(input_file: str, output_file: str) -> None:
-    """Canonicalize SMILES strings (molecules or reactions) contained in a file"""
+def canonicalize() -> None:
+    """
+    Canonicalize SMILES strings (molecules or reactions).
 
-    with open(input_file, 'rt') as fin, open(output_file, 'wt') as fout:
-        for line in fin:
-            smiles = line.strip()
+    The script will read strings either from stdin, or from a file given as the
+    first argument (behavior from the fileinput package)."""
 
-            if '>' in smiles:
-                canonical_smiles = canonicalize_reaction_smiles(smiles)
-            else:
-                canonical_smiles = canonicalize_smiles(smiles)
+    for line in fileinput.input():
+        smiles = line.strip()
 
-            fout.write(f'{canonical_smiles}\n')
+        if '>' in smiles:
+            canonical_smiles = canonicalize_reaction_smiles(smiles)
+        else:
+            canonical_smiles = canonicalize_smiles(smiles)
+
+        print(canonical_smiles)
 
 
 if __name__ == '__main__':
