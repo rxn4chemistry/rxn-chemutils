4569ad2e20af317ae33e8cd5548336b56043eeaa
Matteo Manica <tte@zurich.ibm.com>
2020-11-05 07:21:36 +0100

    chore: removing redefintion of RDKitError.
    Signed-off-by: Matteo Manica <tte@zurich.ibm.com>

--- rxn_chemutils/rdkit_utils.py
+++ rxn_chemutils/rdkit_utils.py
@@ -3,22 +3,6 @@ from typing import Sequence
 from rdkit.Chem.rdchem import Mol
 
 
-class RDKitError(ValueError):
-    """Exception raised in RDKit."""
-
-    def __init__(self, title: str, detail: str):
-        """
-        Initialize RDKitError.
-
-        Args:
-            title (str): title of the error.
-            detail (str): decscription of the error.
-        """
-        self.type = 'RDKitError'
-        self.title = title
-        self.detail = detail
-
-
 def remove_atom_mapping(mols: Sequence[Mol]) -> None:
     for mol in mols:
         for atom in mol.GetAtoms():
