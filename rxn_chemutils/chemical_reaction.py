aa16b9081971cee6de8a7442a60e326394801ae7
Daniel Probst <danuprobst@gmail.com>
2020-11-30 09:13:34 +0100

    Added correct annotaiton for remove (@overload)

--- rxn_chemutils/chemical_reaction.py
+++ rxn_chemutils/chemical_reaction.py
@@ -1,7 +1,7 @@
 """ Contains the class Reaction representing unidirectional reactions. """
 import re
 from enum import Enum
-from typing import Tuple, List, Dict, Any
+from typing import Tuple, List, Dict, Any, overload
 from rdkit.Chem import AllChem as rdk
 from rdkit.Chem.rdchem import Mol
 
@@ -255,10 +255,22 @@ class ChemicalReaction:
 
         return []
 
-    # Annotate the return type as Reaction... Python...
+    @overload
+    def remove(self, indices: Tuple[List[int]]) -> "ChemicalReaction":
+        ...
+
+    @overload
+    def remove(self, indices: Tuple[List[int],
+                                    List[int]]) -> "ChemicalReaction":
+        ...
+
+    @overload
     def remove(
             self, indices: Tuple[List[int], List[int],
                                  List[int]]) -> "ChemicalReaction":
+        ...
+
+    def remove(self, indices):
         """Remove reactants, agents and products based on their index within the respective lists.
 
         Args:
