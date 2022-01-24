24d925d94cd736ebd2cdb7116b8671cd1b5ec840
Alain Vaucher <AVA@zurich.ibm.com>
2022-01-24 16:21:14 +0100

    function to combine multiple Mols (#47)
    * function to combine multiple Mols
    
    * flake8

--- rxn_chemutils/rdkit_utils.py
+++ rxn_chemutils/rdkit_utils.py
@@ -3,12 +3,13 @@
 # (C) Copyright IBM Corp. 2021
 # ALL RIGHTS RESERVED
 
-from typing import Sequence
+from typing import Iterable
 
+from rdkit.Chem import CombineMols
 from rdkit.Chem.rdchem import Mol
 
 
-def clear_atom_mapping(mols: Sequence[Mol]) -> None:
+def clear_atom_mapping(mols: Iterable[Mol]) -> None:
     """
     Remove the mapping information in RDKit Mol objects.
     """
@@ -16,3 +17,18 @@ def clear_atom_mapping(mols: Sequence[Mol]) -> None:
         for atom in mol.GetAtoms():
             if atom.HasProp('molAtomMapNumber'):
                 atom.ClearProp('molAtomMapNumber')
+
+
+def combine_mols(mols: Iterable[Mol]) -> Mol:
+    """Combine multiple RDKit Mols into one.
+
+    RDKit has a function for that but it only supports two mols.
+
+    Args:
+        mols: RDKit Mols to combine.
+    """
+
+    resulting_mol = Mol()
+    for mol in mols:
+        resulting_mol = CombineMols(resulting_mol, mol)
+    return resulting_mol
---
+++ rxn_chemutils/tests/test_rdkit_utils.py
@@ -0,0 +1,24 @@
+from rxn_chemutils.conversion import mol_to_smiles, smiles_to_mol
+from rxn_chemutils.rdkit_utils import combine_mols
+
+
+def test_combine_mols():
+    # No molecule -> empty Mol
+    assert mol_to_smiles(combine_mols([])) == ''
+
+    # Test a few single mols
+    for smiles in ['CC', 'CCO', 'CC(C)CCO']:
+        mol = smiles_to_mol(smiles)
+        assert mol_to_smiles(combine_mols([mol])) == smiles
+
+    # Test a few combinations of molecules
+    for multi_smiles in [
+        ('CC', 'O'),
+        ('CC', 'CCO', 'N'),
+        ('CC', 'CC', 'CC(C)CCO', 'c1ccccc1'),
+    ]:
+        mols = (smiles_to_mol(smiles) for smiles in multi_smiles)
+        merged_mol = combine_mols(mols)
+        # we verify the merge by checking that the conversion back to SMILES
+        # leads to the '.'-concatenated separate SMILES.
+        assert mol_to_smiles(merged_mol) == '.'.join(multi_smiles)
