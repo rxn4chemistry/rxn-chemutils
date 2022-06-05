9377426d6b496b1ad3aa8298827a113af1e84025
Alain Vaucher <AVA@zurich.ibm.com>
2022-06-05 15:35:52 +0200

    Move package to src dir (#62)

--- src/rxn_chemutils/reaction_equation.py
+++ src/rxn_chemutils/reaction_equation.py
@@ -50,9 +50,9 @@ class ReactionEquation:
         Overwrite init function in order to enable instantiation from any iterator and
         to force copying the lists.
         """
-        self.__attrs_init__(
+        self.__attrs_init__(  # type: ignore
             list(reactants), list(agents), list(products)
-        )  # type: ignore
+        )
 
     def __iter__(self) -> Iterator[List[str]]:
         """Helper function to simplify functionality acting on all three
