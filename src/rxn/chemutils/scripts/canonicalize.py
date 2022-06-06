1818a389af25093408b9999c991a8bba9de6dc2c
Alain Vaucher <AVA@zurich.ibm.com>
2022-06-06 14:37:30 +0200

    Rewrite scripts (#65)
    * Rewrite scripts
    
    * remove line in setup.cfg
    
    * simplify
    
    * fix
    
    * Extension to canonicalize script
    
    * update doc
    
    * isort

--- src/rxn/chemutils/scripts/canonicalize.py
+++ src/rxn/chemutils/scripts/canonicalize.py
@@ -1,5 +1,46 @@
+import sys
+from typing import Optional, TextIO
+
+import click
+
+from rxn.chemutils.miscellaneous import canonicalize_any
+
+
+@click.command()
+@click.argument("input_file", type=click.File(mode="r"), default=sys.stdin)
+@click.argument("output_file", type=click.File(mode="w"), default=sys.stdout)
+@click.option(
+    "--invalid_placeholder",
+    type=str,
+    help=(
+        "If specified, the given value will be the output for invalid input SMILES. "
+        "By default, an exception is raised in such cases."
+    ),
+)
+def main(
+    input_file: TextIO, output_file: TextIO, invalid_placeholder: Optional[str]
+) -> None:
+    """
+    Canonicalize SMILES strings (molecules, sets of molecules, or reactions).
+
+    The script will read strings either from stdin, or from a file given as the
+    first argument, and write to stdout, or from a file given as the second
+    argument.
+    """
+
+    for line in input_file:
+        smiles = line.strip()
+
+        # Canonicalize the SMILES, handle exception if needed
+        try:
+            canonical = canonicalize_any(smiles)
+        except Exception:
+            if invalid_placeholder is None:
+                raise
+            canonical = invalid_placeholder
+
+        output_file.write(f"{canonical}\n")
+
+
 if __name__ == "__main__":
-    raise SystemExit(
-        "Does not exist anymore. "
-        "Use chemutils-canonicalize script instead (installed along the rxn.chemutils package)."
-    )
+    main()
--- src/rxn/chemutils/scripts/detokenize.py
+++ src/rxn/chemutils/scripts/detokenize.py
@@ -1,5 +1,27 @@
+import sys
+from typing import TextIO
+
+import click
+
+from rxn.chemutils.tokenization import detokenize_smiles
+
+
+@click.command()
+@click.argument("input_file", type=click.File(mode="r"), default=sys.stdin)
+@click.argument("output_file", type=click.File(mode="w"), default=sys.stdout)
+def main(input_file: TextIO, output_file: TextIO) -> None:
+    """
+    Detokenize SMILES strings (molecules or reactions).
+
+    The script will read strings either from stdin, or from a file given as the
+    first argument, and write to stdout, or from a file given as the second
+    argument.
+    """
+
+    for line in input_file:
+        smiles = line.strip()
+        output_file.write(f"{detokenize_smiles(smiles)}\n")
+
+
 if __name__ == "__main__":
-    raise SystemExit(
-        "Does not exist anymore. "
-        "Use chemutils-detokenize script instead (installed along the rxn.chemutils package)."
-    )
+    main()
