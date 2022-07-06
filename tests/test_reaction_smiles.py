6ff061573aa917fef6595fff6278b279138f3dc6
Amol Thakkar <av.mol.th@gmail.com>
2022-07-06 11:36:40 +0200

    added tests for new extended SMILES condition, changed condition to |

--- tests/test_reaction_smiles.py
+++ tests/test_reaction_smiles.py
@@ -18,6 +18,20 @@ def test_determine_format():
         " |c:5,7,H:3.2,&1:4,24,f:3.4|"
     )
 
+    reaction_smiles_with_other_extended_information_2 = (
+        "[CH3:1][C:2](=[O:3])[NH:4][C@H:5]([C:6](=[O:7])[OH:8])[C:9]([CH3:10])"
+        "([CH3:11])[SH:12].[O:13]=[N:14]O[Na]>O.Cl.CO>[CH3:1][C:2](=[O:3])[NH:4]"
+        "[C@H:5]([C:6](=[O:7])[OH:8])[C:9]([CH3:10])([CH3:11])[S:12][N:14]=[O:13]"
+        " |c:5,7,H:3.2,&1:4,24|"
+    )
+
+    reaction_smiles_with_other_extended_information_3 = (
+        "[CH3:1][C:2](=[O:3])[NH:4][C@H:5]([C:6](=[O:7])[OH:8])[C:9]([CH3:10])"
+        "([CH3:11])[SH:12].[O:13]=[N:14]O[Na]>O.Cl.CO>[CH3:1][C:2](=[O:3])[NH:4]"
+        "[C@H:5]([C:6](=[O:7])[OH:8])[C:9]([CH3:10])([CH3:11])[S:12][N:14]=[O:13]"
+        " |&1:4,24|"
+    )
+
     assert determine_format("CC.O>>CCO") is ReactionFormat.STANDARD
     assert (
         determine_format("CC.O.[Na+]~[Cl-]>>CCO") is ReactionFormat.STANDARD_WITH_TILDE
@@ -25,6 +39,14 @@ def test_determine_format():
     assert determine_format("CC.O.[Na+].[Cl-]>>CCO |f:2.3|") is ReactionFormat.EXTENDED
     assert (
         determine_format(reaction_smiles_with_other_extended_information)
+        is ReactionFormat.EXTENDED
+    )
+    assert (
+        determine_format(reaction_smiles_with_other_extended_information_2)
+        is ReactionFormat.EXTENDED
+    )
+    assert (
+        determine_format(reaction_smiles_with_other_extended_information_3)
         is ReactionFormat.EXTENDED
     )
 
